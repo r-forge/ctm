aac572d2fa4da832ec2a85313252efb51a59c1be
sasieg (Sandra Siegfried) <siegfried.sandra@protonmail.com>
2022-02-09 14:15:03 +0000

    as.mlt more robust
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@1834 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cotram/R/confband.R
+++ pkg/cotram/R/confband.R
@@ -19,7 +19,7 @@ confband.cotram <- function(object, newdata,  level = 0.95,
         if (any(q < 0)) stop("q is non-positive")
         if (!smooth && !all(q %% 1 == 0)) stop("q is non-integer")
         q <- q + as.integer(object$plus_one) 
-        ## <FIXME> What to do, when lenght(q) > 20? <\FIXME> 
+        ## <FIXME> What to do, when length(q) > 20? <\FIXME> 
     } else {
         q <- mkgrid(object, n = K)[[y]]
         
@@ -49,6 +49,6 @@ confband.cotram <- function(object, newdata,  level = 0.95,
     if (type == "survivor") ci <- 1 - object$model$todistr$p(ci)
     if (type == "cumhazard") ci <- -log(1 - object$model$todistr$p(ci))
     
-    ci <- cbind(q = q - as.integer(object$plus_one) , ci)
+    ci <- cbind(q = q - as.integer(object$plus_one), ci)
     return(ci)
 }
