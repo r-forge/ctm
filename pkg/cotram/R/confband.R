802513eae1f01978efce18e3fd13f3cf0853d9a5
sasieg (Sandra Siegfried) <siegfried.sandra@protonmail.com>
2022-02-15 08:38:47 +0000

    plus_one
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@1840 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cotram/R/confband.R
+++ pkg/cotram/R/confband.R
@@ -15,10 +15,12 @@ confband.cotram <- function(object, newdata,  level = 0.95,
     
     y <- variable.names(object, "response")
     
+    plus_one <- as.integer(object$log_first)
+    
     if (!is.null(q)) {
         if (any(q < 0)) stop("q is non-positive")
         if (!smooth && !all(q %% 1 == 0)) stop("q is non-integer")
-        q <- q + as.integer(object$plus_one) 
+        q <- q + plus_one
         ## <FIXME> What to do, when length(q) > 20? <\FIXME> 
     } else {
         q <- mkgrid(object, n = K)[[y]]
@@ -49,6 +51,6 @@ confband.cotram <- function(object, newdata,  level = 0.95,
     if (type == "survivor") ci <- 1 - object$model$todistr$p(ci)
     if (type == "cumhazard") ci <- -log(1 - object$model$todistr$p(ci))
     
-    ci <- cbind(q = q - as.integer(object$plus_one), ci)
+    ci <- cbind(q = q - plus_one, ci)
     return(ci)
 }
