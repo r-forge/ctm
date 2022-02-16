0b68596bfd218ea27c454ef92685f9193be16ee5
sasieg (Sandra Siegfried) <sandra.siegfried@uzh.ch>
2022-02-16 13:57:16 +0000

    only checks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@1876 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cotram/tests/checks-Ex.R
+++ pkg/cotram/tests/checks-Ex.R
@@ -7,13 +7,11 @@ y <- 0L:100L
 x <- runif(length(y))
 
 yp1 <- y + 1L
-mt <- Coxph(yp1 ~ x)
+d <- data.frame(y = y, x = x)
 
-m <- cotram(y ~ x)
+m <- cotram(y ~ x, log_first = TRUE)
 
 mm <- cotram(y ~ x, log_first = FALSE)
-
-nd <- data.frame(y = y, x = x)
 
 ## ---- checks for y > 0 and y %% 1 == 0 ----
 ym <- -y
@@ -21,65 +19,14 @@ try(cotram(ym ~ x))
 yn <- y/2
 try(cotram(yn ~ x))
 
-try(logLik(m, newdata = data.frame(y = ym, x = x)))
-try(logLik(m, newdata = data.frame(y = yn, x = x)))
+try(logLik(mm, newdata = data.frame(y = ym, x = x)))
+try(logLik(mm, newdata = data.frame(y = yn, x = x)))
 
-try(predict(m, newdata = data.frame(y = ym, x = x)))
-try(predict(m, newdata = data.frame(y = yn, x = x)))
+try(predict(mm, newdata = data.frame(y = ym, x = x)))
+try(predict(mm, newdata = data.frame(y = yn, x = x)))
 
-try(plot(m, newdata = data.frame(y = ym, x = x)))
-try(plot(m, newdata = data.frame(y = yn, x = x)))
+try(plot(mm, newdata = data.frame(y = ym, x = x)))
+try(plot(mm, newdata = data.frame(y = yn, x = x)))
 
-try(plot(m, q = ym, newdata = model.frame(m, x = x)))
-try(plot(m, q = yn, newdata = model.frame(m, x = x)))
-
-## --- checks for plus_one ----
-stopifnot(all(model.frame(m)["y"] == y)) ## model.frame of y for log_first
-
-stopifnot(all(mkgrid(m)$y == y))
-
-stopifnot(logLik(m) == logLik(m, newdata = nd))
-stopifnot(logLik(mm) == logLik(mm, newdata = nd))
-
-## predict
-stopifnot(all.equal(predict(m, type = "density"), predict(m, newdata = data.frame(y = y, x = x), type = "density")))
-stopifnot(all.equal(predict(m, type = "density"), predict(m, newdata = model.frame(m), type = "density")))
-
-ppr <- predict(mm, newdata = nd, type = "density")
-pr <- predict(m, newdata = nd, type = "density")
-
-stopifnot(rownames(pr) == rownames(ppr))
-stopifnot(!any(abs(ppr - pr) > 0.1))
-
-pprq <- predict(mm, newdata = nd, type = "density", q = 0:10)
-prq <- predict(m, newdata = nd, type = "density", q = 0:10)
-
-stopifnot(rownames(prq) == rownames(pprq))
-stopifnot(!any(abs(pprq - prq) > 0.1))
-
-## plot
-if (FALSE) {
-plot(m, newdata = nd,  type = "density", col = 1)
-plot(mm, newdata = nd, type = "density", col = 2, add = TRUE)
-abline(v = 0)
-
-plot(m, newdata = nd,  type = "logdensity", col = 1, ylim = c(-6, 0))
-plot(mm, newdata = nd, type = "logdensity", col = 2, add = TRUE, ylim = c(-6, 0))
-abline(v = 0)
-
-plot(m, newdata = nd, type = "distribution", col = 1)
-plot(mm, newdata = nd, type = "distribution", col = 2, add = TRUE)
-abline(v = 0)
-
-plot(m, newdata = nd, type = "trafo", col = 1)
-plot(mm, newdata = nd, type = "trafo", col = 2, add = TRUE)   
-abline(v = 0)
-
-plot(m, newdata = nd, type = "survivor", col = 1)
-plot(mm, newdata = nd, type = "survivor", col = 2, add = TRUE)   
-abline(v = 0)
-
-plot(m, newdata = nd, type = "cumhazard", col = 1)
-plot(mm, newdata = nd, type = "cumhazard", col = 2, add = TRUE)   
-abline(v = 0)
-}
+try(plot(mm, q = ym, newdata = model.frame(mm)))
+try(plot(mm, q = yn, newdata = model.frame(mm)))
