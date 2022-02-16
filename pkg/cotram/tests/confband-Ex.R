743eb4be39d0ac925d793eb6806d6a3a75b3cd2a
sasieg (Sandra Siegfried) <siegfried.sandra@protonmail.com>
2022-02-16 13:34:46 +0000

    cleanup
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@1874 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cotram/tests/confband-Ex.R
+++ pkg/cotram/tests/confband-Ex.R
@@ -6,41 +6,36 @@ set.seed(25)
 
 
 ## dgp
-dgp <- function(n = 200, seed = 25){
-        set.seed(seed)
-        x <- runif(n, 0, 20)
-        y <- as.integer(rnbinom(n, mu = exp(.2 + .1 * x), size = 3))
-        data.frame(x = x, y)
-}
+n <- 200
+x <- runif(n, 0, 20)
+y <- as.integer(rnbinom(n, mu = exp(.2 + .1 * x), size = 3))
+d <- data.frame(x = x, y)
 
-y <- 1:10
-m1 <- cotram(y ~ x, data = dgp())
+m <- cotram(y ~ x, data = d)
 
+nd <- model.frame(m)[3,]
 
 ## Confband for grid of counts
-confband(m1, type = "distribution", newdata = model.frame(m1)[3,])
+confband(m, type = "distribution", newdata = nd)
 
 ## Confband for K grid points
-confband(m1, type = "distribution", newdata = model.frame(m1)[3, ],
-                 smooth = TRUE, K = 40)
+confband(m, type = "distribution", newdata = nd, smooth = TRUE, K = 40)
 
 
 if (FALSE){
 layout(matrix(1:2, nrow = 1))
 type = "trafo"
-nd <- model.frame(m1)[3,]
-cb <- confband(m1, type =  type, newdata = nd)
-plot(m1, type = type, newdata = nd, 
+
+cb <- confband(m, type =  type, newdata = nd)
+plot(m, type = type, newdata = nd, 
      confidence = "band", col = "red", ylim = c(-2, 15))
 lines(x = cb[, "q"], y = cb[, "lwr"], type = "s")
 lines(x = cb[, "q"], y = cb[, "upr"], type = "s")
 
-cb.s <- confband(m1, type = type, newdata = nd, 
-                 smooth = TRUE)
-plot(m1, type = type, newdata = nd, 
-     confidence = "band", col = "red", smooth = TRUE, ylim = c(-2, 15))
+cb.s <- confband(m, type = type, newdata = nd, smooth = TRUE)
+plot(m, type = type, newdata = nd, smooth = TRUE,
+     confidence = "band", col = "red", ylim = c(-2, 15))
 lines(x = cb.s[, "q"], y = cb.s[, "lwr"], type = "l")
 lines(x = cb.s[, "q"], y = cb.s[, "upr"], type = "l")
 }
-
 
