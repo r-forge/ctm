aff47cb0ecc5e7b4955fd19b4bbfb914414b9512
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2022-11-21 12:18:23 +0000

    comments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@2063 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tram/src/ltmatrices.c
+++ pkg/tram/src/ltmatrices.c
@@ -13,25 +13,38 @@ SEXP R_ltmatrices_solve (SEXP x, SEXP N, SEXP J, SEXP diag)
     double *dx, *dans, *ptr;
     int n, p, info, k, len, j, jj, idx;
 
+    /* number of matrices */
     n = INTEGER(N)[0];
+    /* if (diag)
+           p = J * (J + 1) / 2
+       else 
+           p = J * (J - 1) / 2
+    */
     p = LENGTH(x) / n;
     dx = REAL(x);
     int idiag = LOGICAL(diag)[0];
     char di, lo = 'L';
     
     if (idiag) {
+        /* non-unit diagonal elements */
         di = 'N';
     } else {
+        /* unit diagonal elements */
         di = 'U';
     }
 
+    /* return object: include unit diagnonal elements if idiag == 0 */
     len = n * (p + (1 - idiag) * INTEGER(J)[0]);
     PROTECT(ans = allocVector(REALSXP, len));
     dans = REAL(ans);
-    
+
+    /* loop over matrices, ie rows of x */    
     for (int i = 0; i < n; i++) {
-    
+
+        /* point to subvector of ans */    
         ptr = dans + i * (p + (1 - idiag) * INTEGER(J)[0]);
+
+        /* copy data and insert unit diagonal elements when necessary */
         jj = 0;
         k = 0;
         idx = 0;
@@ -49,11 +62,14 @@ SEXP R_ltmatrices_solve (SEXP x, SEXP N, SEXP J, SEXP diag)
         }
         if (idiag == 0) ptr[idx] = 1.0;
 
+        /* compute inverse */
         F77_CALL(dtptri)(&lo, &di, INTEGER(J), ptr, &info FCONE FCONE);
 
         if (info != 0)
             error("Cannot solve ltmatices");
     }
     UNPROTECT(1);
+
+    /* note: ans always includes diagonal elements */
     return(ans);
 }
