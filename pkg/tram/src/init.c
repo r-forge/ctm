51e841972366579ca85f29ff91d7b8e64810aef8
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2022-11-22 09:37:45 +0000

    add tcrossprod
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@2068 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tram/src/init.c
+++ pkg/tram/src/init.c
@@ -10,11 +10,13 @@
 extern SEXP R_pnormMRS(SEXP);
 extern SEXP R_inner(SEXP, SEXP);
 extern SEXP R_ltmatrices_solve(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP R_ltmatrices_tcrossprod(SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
     {"R_pnormMRS", (DL_FUNC) &R_pnormMRS, 1},
     {"R_inner", (DL_FUNC) &R_inner, 2},
     {"R_ltmatrices_solve", (DL_FUNC) &R_ltmatrices_solve, 5},
+    {"R_ltmatrices_tcrossprod", (DL_FUNC) &R_ltmatrices_tcrossprod, 5},
     {NULL, NULL, 0}
 };
 
