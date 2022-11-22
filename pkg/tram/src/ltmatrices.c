3723fc5466faa1450e781a8fcc693ab4f93f587c
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2022-11-22 09:37:45 +0000

    add tcrossprod
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@2068 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tram/src/ltmatrices.c
+++ pkg/tram/src/ltmatrices.c
@@ -32,16 +32,17 @@ SEXP R_ltmatrices_solve (SEXP A, SEXP b, SEXP N, SEXP J, SEXP diag)
     SEXP ans, ansx;
     double *dA, *dans, *dansx, *ptrA, *ptrb, *db;
     int n, p, info, k, len, j, jj, idx, ONE = 1;
+    int iJ = INTEGER(J)[0];
 
     /* number of matrices */
-    n = INTEGER(N)[0];
+    int iN = INTEGER(N)[0];
     /* p = J * (J - 1) / 2 + diag * J */
-    p = LENGTH(A) / n;
+    p = LENGTH(A) / iN;
     dA = REAL(A);
-    int idiag = LOGICAL(diag)[0];
+    Rboolean Rdiag = asLogical(diag);
     char di, lo = 'L', tr = 'N';
     
-    if (idiag) {
+    if (Rdiag) {
         /* non-unit diagonal elements */
         di = 'N';
     } else {
@@ -49,22 +50,22 @@ SEXP R_ltmatrices_solve (SEXP A, SEXP b, SEXP N, SEXP J, SEXP diag)
         di = 'U';
     }
 
-    /* return object: include unit diagnonal elements if idiag == 0 */
-    len = n * (p + (1 - idiag) * INTEGER(J)[0]);
+    /* return object: include unit diagnonal elements if Rdiag == 0 */
+    len = iN * (p + (1 - Rdiag) * iJ);
     PROTECT(ans = allocVector(REALSXP, len));
     dans = REAL(ans);
 
     if (b != R_NilValue) {
-        PROTECT(ansx = allocVector(REALSXP, n * INTEGER(J)[0]));
+        PROTECT(ansx = allocVector(REALSXP, iN * iJ));
         dansx = REAL(ansx);
         db = REAL(b);
     }
     
     /* loop over matrices, ie rows of x */    
-    for (int i = 0; i < n; i++) {
+    for (int n = 0; n < iN; n++) {
 
         /* point to subvector of ans */    
-        ptrA = dans + i * (p + (1 - idiag) * INTEGER(J)[0]);
+        ptrA = dans + n * (p + (1 - Rdiag) * iJ);
 
         /* copy data and insert unit diagonal elements when necessary */
         jj = 0;
@@ -72,29 +73,29 @@ SEXP R_ltmatrices_solve (SEXP A, SEXP b, SEXP N, SEXP J, SEXP diag)
         idx = 0;
         j = 0;
         while(j < p) {
-            if (idiag == 0 && (jj == idx)) {
+            if (!Rdiag && (jj == idx)) {
                 ptrA[jj] = 1.0;
-                idx = idx + (INTEGER(J)[0] - k);
+                idx = idx + (iJ - k);
                 k++;
             } else {
-                ptrA[jj] = dA[j * n + i];
+                ptrA[jj] = dA[j * iN + n];
                 j++;
             }
             jj++;
         }
-        if (idiag == 0) ptrA[idx] = 1.0;
+        if (!Rdiag) ptrA[idx] = 1.0;
 
         if (b == R_NilValue) {
             /* compute inverse */
-            F77_CALL(dtptri)(&lo, &di, INTEGER(J), ptrA, &info FCONE FCONE);
+            F77_CALL(dtptri)(&lo, &di, &iJ, ptrA, &info FCONE FCONE);
             if (info != 0)
                 error("Cannot solve ltmatices");
         } else {
             /* solve linear system */
-            ptrb = dansx + i * INTEGER(J)[0];
-            for (j = 0; j < INTEGER(J)[0]; j++)
-                ptrb[j] = db[i * INTEGER(J)[0] + j];
-            F77_CALL(dtpsv)(&lo, &tr, &di, INTEGER(J), ptrA, ptrb, &ONE FCONE FCONE);
+            ptrb = dansx + n * iJ;
+            for (j = 0; j < iJ; j++)
+                ptrb[j] = db[n * iJ + j];
+            F77_CALL(dtpsv)(&lo, &tr, &di, &iJ, ptrA, ptrb, &ONE FCONE FCONE);
         }
     }
     
@@ -106,4 +107,69 @@ SEXP R_ltmatrices_solve (SEXP A, SEXP b, SEXP N, SEXP J, SEXP diag)
         UNPROTECT(2);
         return(ansx);
     }
+}
+
+#define IDX(i, j, n, d) ((i) >= (j) ? (n) * ((j) - 1) - ((j) - 2) * ((j) - 1)/2 + (i) - (j) - (!d) * (j) : 0)
+
+SEXP R_ltmatrices_tcrossprod (SEXP A, SEXP N, SEXP J, SEXP diag, SEXP diag_only) {
+
+    SEXP ans;
+    int iJ = INTEGER(J)[0];
+    int iN = INTEGER(N)[0];
+    int i, j, k, ix;
+    double *dans, *dA;
+    Rboolean Rdiag_only = asLogical(diag_only);
+    Rboolean Rdiag = asLogical(diag);
+
+    dA = REAL(A);
+    
+    if (Rdiag_only) {
+        PROTECT(ans = allocVector(REALSXP, iN * iJ));
+        dans = REAL(ans);
+        for (int n = 0; n < iN; n++) {
+            dans[n] = 1.0;
+            if (Rdiag) dans[n] = pow(dA[n], 2);
+            for (i = 1; i < iJ; i++) {
+                ix = i * iN + n;
+                dans[ix] = 0.0;
+                for (k = 0; k < i; k++)
+                    dans[ix] += pow(dA[IDX(i + 1, k + 1, iJ, Rdiag) * iN + n], 2);
+                if (Rdiag) {
+                    dans[ix] += pow(dA[IDX(i + 1, i + 1, iJ, Rdiag) * iN + n], 2);
+                } else {
+                    dans[ix] += 1.0;
+                }
+            }
+        }
+    } else {
+        PROTECT(ans = allocVector(REALSXP, iN * iJ * (iJ + 1) / 2));
+        dans = REAL(ans);
+        for (int n = 0; n < INTEGER(N)[0]; n++) {
+            dans[n] = 1.0;
+            if (Rdiag) dans[n] = pow(dA[n], 2);
+            for (i = 1; i < iJ; i++) {
+                for (j = 0; j <= i; j++) {
+                    ix =  IDX(i + 1, j + 1, iJ, 1) * iN + n;
+                    dans[ix] = 0.0;
+                    for (k = 0; k < j; k++)
+                        dans[ix] += 
+                            dA[IDX(i + 1, k + 1, iJ, Rdiag) * iN + n] *
+                            dA[IDX(j + 1, k + 1, iJ, Rdiag) * iN + n];
+                    if (Rdiag) {
+                        dans[ix] += 
+                            dA[IDX(i + 1, j + 1, iJ, Rdiag) * iN + n] *
+                            dA[IDX(j + 1, j + 1, iJ, Rdiag) * iN + n];
+                    } else {
+                        if (j < i) {
+                            dans[ix] += dA[IDX(i + 1, j + 1, iJ, Rdiag) * iN + n];
+                        } else {
+                            dans[ix] += 1.0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    UNPROTECT(1);
+    return(ans);
 }
