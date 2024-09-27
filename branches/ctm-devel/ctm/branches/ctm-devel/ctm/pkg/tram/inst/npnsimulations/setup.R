8cabb1169b28a94e386edff645de7219cf3a9aec
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2024-09-27 11:04:52 +0000

    remove
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@2503 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/ctm-devel/ctm/branches/ctm-devel/ctm/pkg/tram/inst/npnsimulations/setup.R
+++
@@ -1,5 +0,0 @@
-
-### running at 3 Gen Intel(R) Core(TM) i5-1335U cores, the whole 
-### simulation takes approximately 8 hours to complete
-library("parallel")
-MC <- floor(detectCores() / 4)
--- branches/ctm-devel/ctm/pkg/trtf/inst/sim/README.txt
+++
@@ -1,7 +0,0 @@
-
-	Predictive Distribution Modelling Using Transformation Forests
-	Torsten Hothorn and Achim Zeileis
-
-	Simulation Experiments
-
-To reproduce the figures, run ``runsim.R''.
