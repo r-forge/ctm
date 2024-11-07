45b169e24cd9dccd116d13607189deba5912b622
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2024-11-07 13:26:00 +0000

    add npb
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@2538 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/tram_devel/demo/hcc.R
+++ branches/tram_devel/demo/hcc.R
@@ -5,7 +5,9 @@
 ### tests with application to hepatocellular carcinoma diagnosis
 ###
 ### Hepatocellular Carcinoma Diagnosis
+pdf("hcc.pdf")
 source("https://gitlab.com/asewak/optcomb/-/raw/main/hcc.R?ref_type=heads&inline=false", echo = TRUE)
+dev.off()
 ###
 ### Simulation; takes a while
 if (FALSE) 
