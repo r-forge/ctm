c60ba867e45d2137a8e3238f91ac561d7539527d
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2024-11-12 14:59:10 +0000

    add skeleton for mcn
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@2547 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tram/demo/mnc.R
@@ -0,0 +1,13 @@
+
+### Reproducibility material for
+###
+### Marginal Inference under Non-collapsibility
+###
+### Data Analyses
+pdf("mnc.pdf")
+source("https://gitlab.uzh.ch/susanne.dandl1/marginal_noncollapsibility/-/raw/main/application.R?ref_type=heads&inline=false", echo = TRUE)
+dev.off()
+###
+### Simulation; takes a while
+if (FALSE) 
+   source("https://gitlab.uzh.ch/susanne.dandl1/marginal_noncollapsibility/-/raw/main/sim.R?ref_type=heads&inline=false", echo = TRUE)
