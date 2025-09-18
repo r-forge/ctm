70e086e55565170047b86f8bd64ffdbcd1969cc0
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2025-09-18 15:13:32 +0000

    add check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ctm@2652 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tram/tests/Polr-Ex.R
+++ pkg/tram/tests/Polr-Ex.R
@@ -20,3 +20,22 @@ cmp(coef(house.plr), coef(house.plr2))
 ll <- logLik(house.plr)
 attr(ll, "nobs") <- NULL
 cmp(ll, logLik(house.plr2))
+
+set.seed(129)
+
+if (require("TH.data")) {
+
+    ### blood loss data
+    load(system.file("rda", "bloodloss.rda", package = "TH.data"))
+    sMBL <- sort(unique(blood$MBL))
+    blood$MBLc <- cut(blood$MBL, breaks = c(-Inf, sMBL), ordered_result = TRUE)
+
+    op <- mltoptim()
+    ### spg: worked
+    m2 <- Polr(MBLc ~ 1, data = blood, method = "probit", optim = op[2])
+    ### constrOptim: changed defaults in mlt 1.6-7
+    m4 <- Polr(MBLc ~ 1, data = blood, method = "probit", optim = op[4])
+
+    cmp(logLik(m2), logLik(m4))
+
+}
